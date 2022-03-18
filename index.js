af5cb49b00d2accbc0d7c79f8389c40532492348
Jonathan Gomes <jonathan.gomes@hiplatform.com>
2022-03-18 13:42:21 -0300

    add new routes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3333;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/timeout", (req, res) => {
+  const { timeout } = req.query;
+  setTimeout(() => {
+    res.send("Timeout response");
+  }, timeout);
+});
+
+app.get("/server_error", (req, res) => {
+  res.status(500).send();
+});
+
+app.get("/not_found", (req, res) => {
+  res.status(404).send();
 });
 
 app.listen(port, () => {
